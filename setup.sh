b962eead0c7b60ba716550dba6489de2b3939ee7
Mateus Dubiela Oliveira <mateusduboli@gmail.com>
2016-09-18 22:29:41 -0300

    Fix shellchecker lint warnings

--- setup.sh
+++ setup.sh
@@ -11,31 +11,31 @@ HELP+="\n$1\t\t$2"
 
 append_help "usage" "Prints this text."
 function show_help {
-printf "$HELP\n"
+printf "%s" "$HELP"
 }
 
 function safe_link {
 filename=$1
 src="$DOT_FILES/$filename"
 dst="$HOME/.$filename"
-if [[ -L $dst ]] && [[ "$(readlink $dst)" == "$src" ]]; then
+if [[ -L $dst ]] && [[ "$(readlink "$dst")" == "$src" ]]; then
     echo "Link to $dst already made"
     return
 fi
 if [[ -e $dst ]] && [[ ! -L $dst ]]; then
     bak="$BACKUP/$filename"
     echo "Saving $dst to $bak"
-    mv $dst $bak
+    mv "$dst" "$bak"
 fi
 if [[ $OS == 'Linux' ]]; then
     if [[ -d $dst ]]; then
         bak="$BACKUP/$filename"
         echo "Saving $dst to $bak"
-        mv $dst $bak
+        mv "$dst" "$bak"
     fi
 fi
 echo "Linking $src to $dst"
-ln $LN_OPTS $src $dst
+ln $LN_OPTS "$src" "$dst"
 }
 
 function safe_copy {
@@ -48,12 +48,12 @@ dst="$dst_dir/$filename"
 
 if [[ ! -d $dst_dir ]]; then
     echo "Creating $dst_dir."
-    mkdir -p $dst_dir
+    mkdir -p "$dst_dir"
 fi
 
 if [[ ! -e $dst ]]; then
     echo "Copying $src to $dst"
-    cp $src $dst
+    cp  "$src" "$dst"
 else
     echo "$dst already there."
 fi
@@ -62,7 +62,7 @@ fi
 function safe_install {
 command=$1
 echo "Checking $command"
-which $command &> /dev/null
+which "$command" &> /dev/null
 if [[ $? == 0 ]]; then
     return 0
 else
@@ -75,7 +75,7 @@ append_help "zsh" "Installs oh-my-zsh and link my custom folder."
 function install_zsh {
 if safe_install 'zsh'; then
     if [[ ! -e "$HOME/.oh-my-zsh" ]]; then
-        curl -L --progress-bar $OH_MY_ZSH_URL | sh
+        curl -L --progress-bar "$OH_MY_ZSH_URL" | sh
     else
         echo "oh-my-zsh already installed."
     fi
@@ -90,7 +90,7 @@ function install_vim {
 if  safe_install 'vim' ; then
     VUNDLE_HOME=$HOME/.vim/bundle/vundle
     if [[ ! -e "$VUNDLE_HOME" ]]; then
-        git clone $VUNDLE_URL $VUNDLE_HOME &>/dev/null
+        git clone "$VUNDLE_URL" "$VUNDLE_HOME" &>/dev/null
     else
         echo "Vundle already installed."
     fi
@@ -107,7 +107,7 @@ function install_nvim {
 if  safe_install 'nvim' ; then
     VUNDLE_HOME="$HOME/.nvim/bundle/vundle"
     if [[ ! -e "$VUNDLE_HOME" ]]; then
-        git clone $VUNDLE_URL $VUNDLE_HOME >/dev/null
+        git clone "$VUNDLE_URL" "$VUNDLE_HOME"  >/dev/null
         echo "Vundle installed."
     else
         echo "Vundle already installed."
@@ -125,7 +125,7 @@ function install_fonts {
 if  safe_install 'fc-cache' ; then
     IFS=$(printf '\t\n\r')
     font_dir="fonts"
-    for font in $(ls -1 "$font_dir"); do
+    for font in $font_dir; do
         safe_copy $font_dir $font
     done
     fc-cache "$HOME/.fonts"
@@ -161,7 +161,7 @@ if  safe_install 'rvm' ; then
     RVM_KEYID='409B6B1796C275462A1703113804BB82D39DC0E3'
     gpg --keyserver $RVM_KEYSERVER \
         --recv-keys $RVM_KEYID
-    \curl -sSL https://get.rvm.io | bash
+    curl -sSL https://get.rvm.io | bash
 fi
 }
 
@@ -188,7 +188,7 @@ esac
 }
 
 os_opts
-mkdir $BACKUP 2>/dev/null
+mkdir "$BACKUP" 2>/dev/null
 case "$1" in
     'zsh')
         install_zsh
