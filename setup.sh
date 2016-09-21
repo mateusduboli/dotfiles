d11d691641828db939c09601997604443c989fb7
Mateus Dubiela Oliveira <mateusduboli@gmail.com>
2016-09-20 22:17:17 -0300

    Use consistent identation

--- setup.sh
+++ setup.sh
@@ -6,16 +6,16 @@ LN_OPTS='-s'
 OS=$(uname)
 
 function append_help {
-HELP+="$1\t\t$2\n"
+  HELP+="$1\t\t$2\n"
 }
 
 append_help "usage" "Prints this text."
 function show_help {
-echo -ne "$HELP"
+  echo -ne "$HELP"
 }
 
 function backup_file {
-  local FILENAME=$1
+  local DST=$1
   local BAK="$BACKUP/$FILENAME"
   echo "Backing up $DST to $BAK"
   mv "$DST" "$BAK"
@@ -31,96 +31,90 @@ function link_to_config {
     return
   fi
   if [[ -e "$DST" ]] && [[ ! -L "$DST" ]]; then
-    backup_file "$FILENAME"
+    backup_file "$DST"
   fi
   echo "Linking $SRC to $DST"
   ln "$LN_OPTS" "$SRC" "$DST"
 }
 
 function safe_link {
-filename=$1
-src="$DOT_FILES/$filename"
-dst="$HOME/.$filename"
-if [[ -L $dst ]] && [[ "$(readlink "$dst")" == "$src" ]]; then
-    echo "Link to $dst already made"
+  local FILENAME=$1
+  local SRC="$DOT_FILES/$FILENAME"
+  local DST="$HOME/.$FILENAME"
+
+  if [[ -L $DST ]] && [[ "$(readlink "$DST")" == "$SRC" ]]; then
+    echo "Link to $DST already made"
     return
-fi
-if [[ -e $dst ]] && [[ ! -L $dst ]]; then
-    bak="$BACKUP/$filename"
-    echo "Saving $dst to $bak"
-    mv "$dst" "$bak"
-fi
-if [[ $OS == 'Linux' ]]; then
-    if [[ -d $dst ]]; then
-        bak="$BACKUP/$filename"
-        echo "Saving $dst to $bak"
-        mv "$dst" "$bak"
-    fi
-fi
-echo "Linking $src to $dst"
-ln $LN_OPTS "$src" "$dst"
+  fi
+  if [[ -e $DST ]] && [[ ! -L $DST ]]; then
+    backup_file "$DST"
+  fi
+  echo "Linking $SRC to $DST"
+  ln "$LN_OPTS" "$SRC" "$DST"
 }
 
 function safe_copy {
-directory=$1
-filename=$2
+  local DIRECTORY=$1
+  local FILENAME=$2
 
-src="$PWD/$directory/$filename"
-dst_dir="$HOME/.$directory"
-dst="$dst_dir/$filename"
+  local SRC="$PWD/$DIRECTORY/$FILENAME"
+  local DST_DIR="$HOME/.$DIRECTORY"
+  local DST="$DST_DIR/$FILENAME"
 
-if [[ ! -d $dst_dir ]]; then
-    echo "Creating $dst_dir."
-    mkdir -p "$dst_dir"
-fi
+  if [[ ! -d $DST_DIR ]]; then
+    echo "Creating $DST_DIR."
+    mkdir -p "$DST_DIR"
+  fi
 
-if [[ ! -e $dst ]]; then
-    echo "Copying $src to $dst"
-    cp  "$src" "$dst"
-else
-    echo "$dst already there."
-fi
+  if [[ ! -e $DST ]]; then
+    echo "Copying $SRC to $DST"
+    cp  "$SRC" "$DST"
+  else
+    echo "$DST already there."
+  fi
 }
 
-function safe_install {
-command=$1
-echo "Checking $command"
-which "$command" &> /dev/null
-if [[ $? == 0 ]]; then
+function check_executable {
+  local COMMAND=$1
+
+  echo "Checking $COMMAND"
+  which "$COMMAND" &> /dev/null
+  if [[ $? == 0 ]]; then
     return 0
-else
-    echo "First install $command."
+  else
+    echo "First install $COMMAND."
     return 1
-fi
+  fi
 }
 
 append_help "zsh" "Installs oh-my-zsh and link my custom folder."
 function install_zsh {
-if safe_install 'zsh'; then
+  if check_executable 'zsh'; then
+
     if [[ ! -e "$HOME/.oh-my-zsh" ]]; then
-        curl -L --progress-bar "$OH_MY_ZSH_URL" | sh
+      curl -L --progress-bar "$OH_MY_ZSH_URL" | sh
     else
-        echo "oh-my-zsh already installed."
+      echo "oh-my-zsh already installed."
     fi
     safe_link 'zshrc'
     safe_link 'zsh'
     safe_link 'zsh.alias'
-fi
+  fi
 }
 
 function install_nvim_vundle {
   if [[ ! -e "$VUNDLE_HOME" ]]; then
-      git clone "$VUNDLE_URL" "$VUNDLE_HOME"  >/dev/null
-      echo "Vundle installed."
+    git clone "$VUNDLE_URL" "$VUNDLE_HOME"  >/dev/null
+    echo "Vundle installed."
   else
-      echo "Vundle already installed."
+    echo "Vundle already installed."
   fi
   nvim +PluginInstall +qa
 }
 
 append_help "nvim" "Installs vundle, install my plugins and link my nvimrc and my nvim folder"
 function install_nvim {
-  if  safe_install 'nvim' ; then
+  if  check_executable 'nvim' ; then
     link_to_config 'nvim'
     install_nvim_vundle
   fi
@@ -129,110 +123,108 @@ function install_nvim {
 function install_nvim_vundle {
   VUNDLE_HOME="$HOME/.config/nvim/bundle/Vundle.vim"
   if [[ ! -e "$VUNDLE_HOME" ]]; then
-      git clone "$VUNDLE_URL" "$VUNDLE_HOME"  >/dev/null
-      echo "Vundle installed."
+    git clone "$VUNDLE_URL" "$VUNDLE_HOME"  >/dev/null
+    echo "Vundle installed."
   else
-      echo "Vundle already installed."
+    echo "Vundle already installed."
   fi
   nvim +PluginInstall +qa
 }
 
 append_help "fonts" "Copy 'Source Code Pro For Powerline' to .fonts, and runs fcache"
 function install_fonts {
-if  safe_install 'fc-cache' ; then
+  if  check_executable 'fc-cache' ; then
     IFS=$(printf '\t\n\r')
     font_dir="fonts"
     for font in $font_dir; do
-        safe_copy $font_dir $font
+      safe_copy $font_dir $font
     done
     fc-cache "$HOME/.fonts"
-fi
+  fi
 }
 
 append_help "git" "Links my gitconfig."
 function install_git {
-if  safe_install 'git' ; then
+  if  check_executable 'git' ; then
     safe_link 'gitconfig'
     safe_link 'git-commit-template.txt'
-fi
+  fi
 }
 
 append_help "tmux" "Links tmux.conf"
 function install_tmux {
-if  safe_install 'tmux' ; then
+  if  check_executable 'tmux' ; then
     safe_link 'tmux.conf'
-    if safe_install 'mux' ; then
-        echo "Installing tmuxnator"
-        gem install tmuxinator
+    if check_executable 'mux' ; then
+      echo "Installing tmuxnator"
+      gem install tmuxinator
     else
-        echo "Tmuxinator already installed"
+      echo "Tmuxinator already installed"
     fi
-fi
+  fi
 
 }
 
 append_help "rvm" "Install rvm, the ruby version manager"
 function install_rvm {
-if  safe_install 'rvm' ; then
-    RVM_KEYSERVER='hkp://keys.gnupg.net'
-    RVM_KEYID='409B6B1796C275462A1703113804BB82D39DC0E3'
-    gpg --keyserver $RVM_KEYSERVER \
-        --recv-keys $RVM_KEYID
+  if  check_executable 'rvm' ; then
+    gpg --keyserver "$RVM_KEYSERVER" \
+      --recv-keys "$RVM_KEYID"
     curl -sSL https://get.rvm.io | bash
-fi
+  fi
 }
 
 append_help "all" "Runs all the above."
 function install_all {
-echo "Installing all."
-install_zsh
-install_nvim
-install_fonts
-install_tmux
-install_rvm
-install_git
+  echo "Installing all."
+  install_zsh
+  install_nvim
+  install_fonts
+  install_tmux
+  install_rvm
+  install_git
 }
 
 function os_opts {
-case "$(uname)" in
+  case "$(uname)" in
     'Linux')
-        LN_OPTS='-sfi'
-        ;;
+      LN_OPTS='-sfi'
+      ;;
     'Darwin')
-        LN_OPTS='-shi'
-        ;;
-esac
+      LN_OPTS='-shi'
+      ;;
+  esac
 }
 
 os_opts
 mkdir "$BACKUP" 2>/dev/null
 case "$1" in
-    'zsh')
-        install_zsh
-        ;;
-    'vim')
-        install_vim
-        ;;
-    'nvim')
-        install_nvim
-        ;;
-    'fonts')
-        install_fonts
-        ;;
-    'git')
-        install_git
-        ;;
-    'tmux')
-        install_tmux
-        ;;
-    'rvm')
-        install_rvm
-        ;;
-    'all')
-        install_all
-        ;;
-    *)
-        show_help
-        ;;
+  'zsh')
+    install_zsh
+    ;;
+  'vim')
+    install_vim
+    ;;
+  'nvim')
+    install_nvim
+    ;;
+  'fonts')
+    install_fonts
+    ;;
+  'git')
+    install_git
+    ;;
+  'tmux')
+    install_tmux
+    ;;
+  'rvm')
+    install_rvm
+    ;;
+  'all')
+    install_all
+    ;;
+  *)
+    show_help
+    ;;
 esac
 exit 0
